29a006888a237c0aeceefc807d8f9fe55dd25dbf
Felipe Valtl de Mello <valtlfelipe@gmail.com>
2025-08-15 16:23:26 -0300

    fix: cf build

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,11 @@ import path from "path";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig(() => ({
   plugins: [
     react(),
     cloudflare(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
