bf820e221a62a8fc62b600f59d1343450c82263d
Felipe Valtl de Mello <valtlfelipe@gmail.com>
2025-08-17 15:32:21 -0300

    chore: fix umami

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,10 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
-    head: () => ({
-        links: [
-            {
-                rel: 'icon',
-                href: '/favicon.ico',
-            },
-        ],
-        scripts: [
-            {
-                src: 'https://umami.bluelab.cloud/script.js',
-                defer: true,
-                'data-website-id': '4483964e-a22c-4ba7-9857-be25f8b1e55b'
-            },
-        ],
-    }),
     component: () => (
         <>
             <TooltipProvider>
